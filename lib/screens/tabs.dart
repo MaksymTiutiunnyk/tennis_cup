02c12eefdf75d76c3f9bfd85d99afde675ac4bf4
Max <tyutyunnik.maks@gmail.com>
2024-10-31 10:43:22 +0200

    Add titles for pages

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -14,6 +14,7 @@ class Tabs extends StatefulWidget {
 class _Tabs extends State<Tabs> {
   int selectedPageIndex = 0;
   Widget activePage = const Home();
+  String pageTitle = 'Tennis Cup: Home page';
 
   void onSelectPage(int index) {
     setState(() {
@@ -26,19 +27,27 @@ class _Tabs extends State<Tabs> {
     switch (selectedPageIndex) {
       case 0:
         activePage = const Home();
+        pageTitle = 'Tennis Cup: Home page';
         break;
       case 1:
         activePage = const Schedule();
+        pageTitle = 'Tennis Cup: Schedule';
         break;
+      case 2:
+        activePage = const Home();
+        pageTitle = 'Tennis Cup: Ranking';
+      case 3:
+        activePage = const Home();
+        pageTitle = 'Tennis Cup: News';
       default:
         activePage = const Home();
+        pageTitle = 'Tennis Cup: Home page';
     }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 0, 29, 53),
         title: Text(
-          'Tennis Cup: Home page',
-          // activePage.title,
+          pageTitle,
           style: const TextStyle(color: Colors.white),
         ),
       ),
