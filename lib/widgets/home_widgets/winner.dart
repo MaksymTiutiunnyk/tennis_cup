3c383e965a0d6fdbf140e0b6d545a610a7377e41
Max <tyutyunnik.maks@gmail.com>
2024-11-07 17:29:59 +0200

    Add navigation to PlayerDetails screen from Winner widget

--- lib/widgets/home_widgets/winner.dart
+++ lib/widgets/home_widgets/winner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tennis_cup/model/tournament.dart';
 import 'package:intl/intl.dart';
+import 'package:tennis_cup/screens/player_details.dart';
 
 DateFormat formatter = DateFormat('yyyy-MM-dd');
 
@@ -10,54 +11,66 @@ class Winner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8),
-      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.white,
-      ),
-      child: Column(
-        children: [
-          const CircleAvatar(
-            radius: 40,
-            backgroundImage: AssetImage('assets/kurtenko_andrii.png'),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            'Kurtenko Andrii',
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.emoji_events, color: Colors.orange, size: 24),
-              const SizedBox(width: 8),
-              Text(
-                'Men, ${tournament.time.name}',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-          const SizedBox(height: 4),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (ctx) => PlayerDetails(player: tournament.players[0])));
+      },
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            const CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage('assets/kurtenko_andrii.png'),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              tournament.players[0].fullName,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                const Icon(Icons.emoji_events, color: Colors.orange, size: 24),
+                const SizedBox(width: 8),
                 Text(
-                  tournament.arena.title,
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.grey),
-                ),
-                Text(
-                  formatter.format(tournament.date),
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.grey),
+                  'Men, ${tournament.time.name}',
+                  style: Theme.of(context).textTheme.bodyMedium,
                 ),
               ],
             ),
-          ),
-        ],
+            const SizedBox(height: 4),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    tournament.arena.title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(color: Colors.grey),
+                  ),
+                  Text(
+                    formatter.format(tournament.date),
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
