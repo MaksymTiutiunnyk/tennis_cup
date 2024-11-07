40f81103b4407e42af903cd62a41ebd3f0776b57
Max <tyutyunnik.maks@gmail.com>
2024-11-07 17:55:18 +0200

    Add navigation to PlayerDetails screen from Ranking screen

--- lib/widgets/ranking_widgets/ranking_player.dart
+++ lib/widgets/ranking_widgets/ranking_player.dart
@@ -1,108 +1,108 @@
 import 'package:flutter/material.dart';
 import 'package:tennis_cup/model/player.dart';
+import 'package:tennis_cup/screens/player_details.dart';
 
-class RankingPlayer extends StatefulWidget {
+class RankingPlayer extends StatelessWidget {
   final Player player;
   const RankingPlayer({super.key, required this.player});
 
   @override
-  State<RankingPlayer> createState() {
-    return _RankingPlayerState();
-  }
-}
-
-class _RankingPlayerState extends State<RankingPlayer> {
-  @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                const CircleAvatar(
-                  radius: 20,
-                  backgroundImage: AssetImage('assets/kurtenko_andrii.png'),
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  '${widget.player.surname} ${widget.player.name}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Rank Tennis Cup:',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                Text(
-                  (35.6).toString(),
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Rank UTTF:',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                Text(
-                  (35.6).toString(),
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Tournaments:',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                Text(
-                  (1).toString(),
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-            const Divider(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'City, Country:',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                Text(
-                  'Kyiv, Ukraine',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Year of birth:',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                Text(
-                  1995.toString(),
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-          ],
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (ctx) => PlayerDetails(player: player)));
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage('assets/kurtenko_andrii.png'),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    '${player.surname} ${player.name}',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Rank Tennis Cup:',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    (35.6).toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Rank UTTF:',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    (35.6).toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Tournaments:',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    (1).toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+              const Divider(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'City, Country:',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    'Kyiv, Ukraine',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Year of birth:',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    1995.toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
