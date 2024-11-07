e978b8c3fd4193728585b4c923bdd36084014d25
Max <tyutyunnik.maks@gmail.com>
2024-11-07 23:06:14 +0200

    Add navigation to Schedule screen from PlayersMatch widget

--- lib/widgets/comparison_widgets.dart/players_match.dart
+++ lib/widgets/comparison_widgets.dart/players_match.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tennis_cup/model/player.dart';
 import 'package:tennis_cup/model/match.dart';
 import 'package:intl/intl.dart';
+import 'package:tennis_cup/providers/arena_filter_provider.dart';
+import 'package:tennis_cup/providers/schedule_date_provider.dart';
+import 'package:tennis_cup/providers/tab_index_provider.dart';
+import 'package:tennis_cup/providers/time_filter_provider.dart';
+import 'package:tennis_cup/screens/tabs.dart';
 
 DateFormat formatter = DateFormat('yyyy-MM-dd, HH:mm');
 
-class PlayersMatch extends StatefulWidget {
+class PlayersMatch extends ConsumerWidget {
   final Player player1, player2;
   final Match match;
   const PlayersMatch(
@@ -15,44 +21,52 @@ class PlayersMatch extends StatefulWidget {
       required this.match});
 
   @override
-  State<PlayersMatch> createState() {
-    return _PlayersMatchState();
-  }
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InkWell(
+      onTap: () {
+        ref
+            .read(scheduleDateProvider.notifier)
+            .selectDate(match.tournament.date);
+        ref.read(timeFilterProvider.notifier).selectTime(match.tournament.time);
+        ref
+            .read(arenaFilterProvider.notifier)
+            .selectArena(match.tournament.arena);
+        ref.read(tabIndexProvider.notifier).selectTab(1);
 
-class _PlayersMatchState extends State<PlayersMatch> {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              formatter.format(widget.match.dateTime),
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            Row(
-              children: [
-                const Icon(Icons.emoji_events),
-                const SizedBox(width: 8),
-                Text(widget.match.tournament.title),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text(
-              '0 : 3',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(fontWeight: FontWeight.w600),
-            ),
-            Text(
-              '(7-11, 2-11, 9-11)',
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ],
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (ctx) => const Tabs()));
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                formatter.format(match.dateTime),
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.emoji_events),
+                  const SizedBox(width: 8),
+                  Text(match.tournament.title),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text(
+                '0 : 3',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(fontWeight: FontWeight.w600),
+              ),
+              Text(
+                '(7-11, 2-11, 9-11)',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ],
+          ),
         ),
       ),
     );
