6274a532706e16d3a060da6a98a931540382bb93
Max <tyutyunnik.maks@gmail.com>
2024-11-07 22:56:29 +0200

    Add navigation to PlayerDetails screen from PlayerIntro widget

--- lib/widgets/comparison_widgets.dart/player_intro.dart
+++ lib/widgets/comparison_widgets.dart/player_intro.dart
@@ -1,78 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:tennis_cup/custom_icons_icons.dart';
 import 'package:tennis_cup/model/player.dart';
+import 'package:tennis_cup/screens/player_details.dart';
 
-class PlayerIntro extends StatefulWidget {
+class PlayerIntro extends StatelessWidget {
   final Player player;
   const PlayerIntro(this.player, {super.key});
 
   @override
-  State<PlayerIntro> createState() {
-    return _PlayerIntroState();
-  }
-}
-
-class _PlayerIntroState extends State<PlayerIntro> {
-  @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Card(
-        margin: const EdgeInsets.all(8),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 8),
-              const CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage(
-                    'https://tabletennis.setkacup.com/api/Image/setka/480x480/Mi8yMy8yMzA5L2ltYWdlLmpwZw==.jpeg'),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                widget.player.fullName,
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    CustomIcons.medal,
-                    size: 16,
-                    color: Colors.yellow.shade600,
-                  ),
-                  const SizedBox(width: 4),
-                  Text(30.toString()),
-                ],
-              ),
-              const SizedBox(height: 4),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    CustomIcons.medal,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
-                  const SizedBox(width: 4),
-                  Text(20.toString()),
-                ],
-              ),
-              const SizedBox(height: 4),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    CustomIcons.medal,
-                    size: 16,
-                    color: Colors.yellow.shade900,
-                  ),
-                  const SizedBox(width: 4),
-                  Text(17.toString()),
-                ],
-              ),
-            ],
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (ctx) => PlayerDetails(player: player)));
+        },
+        child: Card(
+          margin: const EdgeInsets.all(8),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 8),
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                      'https://tabletennis.setkacup.com/api/Image/setka/480x480/Mi8yMy8yMzA5L2ltYWdlLmpwZw==.jpeg'),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  player.fullName,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      CustomIcons.medal,
+                      size: 16,
+                      color: Colors.yellow.shade600,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(30.toString()),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      CustomIcons.medal,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(20.toString()),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      CustomIcons.medal,
+                      size: 16,
+                      color: Colors.yellow.shade900,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(17.toString()),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
