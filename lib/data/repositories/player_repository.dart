b74cf8823be1e5336eeaff2c3975848719b9b3f7
Max <tyutyunnik.maks@gmail.com>
2024-12-04 12:18:41 +0200

    Create PlayerApi and refactor PlayerRepository to use it

--- lib/data/repositories/player_repository.dart
+++ lib/data/repositories/player_repository.dart
@@ -1,24 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tennis_cup/data/data_providers/player_api.dart';
 import 'package:tennis_cup/data/models/player.dart';
 
 class PlayerRepository {
-  static Future<Map<String, dynamic>> fetchRankingPlayers({
+  final PlayerApi playerApi;
+
+  const PlayerRepository({required this.playerApi});
+
+  Future<Map<String, dynamic>> fetchRankingPlayers({
     required int limit,
     DocumentSnapshot? startAfter,
     Sex? sexFilter,
   }) async {
-    Query query =
-        FirebaseFirestore.instance.collection('players').orderBy('rank_tennis', descending: true).limit(limit);
+    final querySnapshot = await playerApi.fetchRankingPlayers(
+      limit: limit,
+      startAfter: startAfter,
+      sexFilter: sexFilter,
+    );
 
-    if (sexFilter != null && sexFilter != Sex.All) {
-      query = query.where('sex', isEqualTo: sexFilter.name);
-    }
-
-    if (startAfter != null) {
-      query = query.startAfterDocument(startAfter);
-    }
-
-    final querySnapshot = await query.get();
     final players = querySnapshot.docs.map((doc) {
       return Player.fromFirestore(doc);
     }).toList();
