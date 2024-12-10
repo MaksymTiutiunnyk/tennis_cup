5589491ab707b239491fdb260982164c8e199192
Max <tyutyunnik.maks@gmail.com>
2024-12-10 16:04:57 +0200

    Add more arenas

--- lib/data/data_providers/arenas.dart
+++ lib/data/data_providers/arenas.dart
@@ -4,7 +4,18 @@ import 'package:tennis_cup/data/models/arena.dart';
 final List<Arena> arenas = [
   Arena(title: 'Australia', color: Colors.green),
   Arena(title: 'Europe', color: const Color.fromARGB(255, 4, 6, 114)),
-  Arena(title: 'Beijing', color: Colors.purple),
+  Arena(title: 'Beijing', color: Colors.greenAccent),
   Arena(title: 'America', color: Colors.red),
   Arena(title: 'Africa', color: Colors.black),
+  Arena(title: 'Asia', color: Colors.yellowAccent),
+  Arena(title: 'Montreal', color: const Color.fromARGB(255, 30, 118, 8)),
+  Arena(title: 'New Delhi', color: const Color.fromARGB(255, 123, 158, 41)),
+  Arena(title: 'Rio', color: const Color.fromARGB(255, 51, 50, 50)),
+  Arena(title: 'Mexico', color: const Color.fromARGB(255, 95, 15, 10)),
+  Arena(title: 'Rome', color: const Color.fromARGB(255, 172, 52, 9)),
+  Arena(title: 'Paris', color: const Color.fromARGB(255, 29, 186, 214)),
+  Arena(title: 'Prague', color: const Color.fromARGB(255, 189, 190, 135)),
+  Arena(title: 'Seoul', color: Colors.orange),
+  Arena(title: 'Tokyo', color: const Color.fromARGB(255, 162, 0, 191)),
+  Arena(title: 'London', color: const Color.fromARGB(255, 135, 34, 128))
 ];
