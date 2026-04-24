bb21d42ece5c8e0e3cb98f30be4540c52ecace45
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-04-24 21:26:57 +0300

    feat(arena): add id and city to the model

--- lib/data/models/arena.dart
+++ lib/data/models/arena.dart
@@ -3,6 +3,46 @@ import 'package:flutter/material.dart';
 class Arena {
   final String title;
   final Color color;
+  final String? id;
+  final String? city;
 
-  Arena({required this.title, required this.color});
+  Arena({required this.title, required this.color, this.id, this.city});
+
+  factory Arena.fromJson(Map<String, dynamic> json) {
+    return Arena(
+      id: json['id']?.toString(),
+      title: json['name'] as String,
+      color: _colorFromString(json['color'] as String? ?? ''),
+      city: json['city'] as String?,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      other is Arena &&
+      (id != null ? id == other.id : title == other.title);
+
+  @override
+  int get hashCode => id?.hashCode ?? title.hashCode;
+
+  static Color _colorFromString(String value) {
+    switch (value.toUpperCase()) {
+      case 'RED':
+        return Colors.red;
+      case 'GREEN':
+        return Colors.green;
+      case 'BLUE':
+        return Colors.blue;
+      case 'YELLOW':
+        return Colors.yellow;
+      case 'WHITE':
+        return Colors.white;
+      case 'BLACK':
+        return Colors.black;
+      case 'BROWN':
+        return Colors.brown;
+      default:
+        return Colors.grey;
+    }
+  }
 }
