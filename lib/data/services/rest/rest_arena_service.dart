73eb02c6b723a72b6f8b7b40973fcac8bb5fce21
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-04-24 23:40:31 +0300

    refactor(arena): move fromJson from model to service

--- lib/data/services/rest/rest_arena_service.dart
+++ lib/data/services/rest/rest_arena_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:tennis_cup/data/models/arena.dart';
 import 'package:tennis_cup/data/services/abstract/i_arena_service.dart';
 
@@ -12,7 +13,29 @@ class RestArenaService implements IArenaService {
     final response = await _dio.get('/api/v1/arenas');
     final data = response.data as List<dynamic>;
     return data
-        .map((json) => Arena.fromJson(json as Map<String, dynamic>))
+        .map((json) => _arenaFromJson(json as Map<String, dynamic>))
         .toList();
+  }
+
+  static Arena _arenaFromJson(Map<String, dynamic> json) {
+    return Arena(
+      id: json['id']?.toString(),
+      title: json['name'] as String,
+      color: _colorFromString(json['color'] as String? ?? ''),
+      city: json['city'] as String?,
+    );
+  }
+
+  static Color _colorFromString(String value) {
+    switch (value.toUpperCase()) {
+      case 'RED':    return Colors.red;
+      case 'GREEN':  return Colors.green;
+      case 'BLUE':   return Colors.blue;
+      case 'YELLOW': return Colors.yellow;
+      case 'WHITE':  return Colors.white;
+      case 'BLACK':  return Colors.black;
+      case 'BROWN':  return Colors.brown;
+      default:       return Colors.grey;
+    }
   }
 }
