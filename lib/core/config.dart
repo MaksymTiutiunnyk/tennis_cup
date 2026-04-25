116362bd2bf65df18966682b56fee812ad5207a9
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-04-25 18:25:43 +0300

    chore: add config file with microservice urls

---
+++ lib/core/config.dart
@@ -0,0 +1,19 @@
+const playerServiceUrl = String.fromEnvironment(
+  'PLAYER_URL',
+  defaultValue: 'http://localhost:8082',
+);
+
+const tournamentServiceUrl = String.fromEnvironment(
+  'TOURNAMENT_URL',
+  defaultValue: 'http://localhost:8084',
+);
+
+const arenaServiceUrl = String.fromEnvironment(
+  'ARENA_URL',
+  defaultValue: 'http://localhost:8083',
+);
+
+const authServiceUrl = String.fromEnvironment(
+  'AUTH_URL',
+  defaultValue: 'http://localhost:8081',
+);
