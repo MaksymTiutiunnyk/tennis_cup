2d7883c83b3e4e41ec7602328faa2e55e297cc29
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-04-26 21:19:23 +0300

    refactor(player manager): add AuthViewCubit for AuthGate management

--- lib/features/auth/presentation/auth_gate.dart
+++ lib/features/auth/presentation/auth_gate.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tennis_cup/features/auth/presentation/login_screen.dart';
 import 'package:tennis_cup/features/auth/presentation/register_screen.dart';
+import 'package:tennis_cup/logic/cubit/auth_view_cubit.dart';
 
-class AuthGate extends StatefulWidget {
+class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
   @override
-  State<AuthGate> createState() => _AuthGateState();
-}
-
-class _AuthGateState extends State<AuthGate> {
-  bool _showLogin = true;
-
-  @override
   Widget build(BuildContext context) {
-    if (_showLogin) {
-      return LoginContent(
-        onSwitchToRegister: () => setState(() => _showLogin = false),
-      );
-    }
-    return RegisterContent(
-      onSwitchToLogin: () => setState(() => _showLogin = true),
+    return BlocProvider(
+      create: (_) => AuthViewCubit(),
+      child: BlocBuilder<AuthViewCubit, AuthView>(
+        builder: (context, view) {
+          if (view == AuthView.login) {
+            return LoginContent(
+              onSwitchToRegister: () =>
+                  context.read<AuthViewCubit>().showRegister(),
+            );
+          }
+          return RegisterContent(
+            onSwitchToLogin: () => context.read<AuthViewCubit>().showLogin(),
+          );
+        },
+      ),
     );
   }
 }
