f195ea5a5914e910601820b7a4299a69c7e81cad
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-04-26 08:40:22 +0300

    feat(tournament): add universal pagination to PlayersTournamentsCubit

--- lib/logic/cubit/players_tournaments_cubit.dart
+++ lib/logic/cubit/players_tournaments_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:tennis_cup/data/data_providers/tournament_api.dart';
+import 'package:tennis_cup/data/models/page_request.dart';
 import 'package:tennis_cup/data/models/player.dart';
 import 'package:tennis_cup/data/models/tournament.dart';
 import 'package:tennis_cup/data/repositories/tournament_repository.dart';
@@ -8,19 +7,20 @@ import 'package:tennis_cup/data/repositories/tournament_repository.dart';
 part 'players_tournaments_state.dart';
 
 class PlayersTournamentsCubit extends Cubit<PlayersTournamentsState> {
-  final TournamentRepository tournamentRepository =
-      const TournamentRepository(tournamentApi: TournamentApi());
-
+  final TournamentRepository tournamentRepository;
   final Player player1;
   final Player player2;
-  DocumentSnapshot? _lastDocument;
+  PageRequest _currentPage = const PageRequest(page: 0, size: 2);
   bool _hasMore = true;
   bool _isLoading = false;
 
-  PlayersTournamentsCubit(this.player1, this.player2)
-      : super(const PlayersTournamentsState(tournaments: [], isLoading: false));
+  PlayersTournamentsCubit(
+    this.player1,
+    this.player2, {
+    required this.tournamentRepository,
+  }) : super(const PlayersTournamentsState(tournaments: [], isLoading: false));
 
-  Future<void> fetchTournaments({int limit = 2}) async {
+  Future<void> fetchTournaments() async {
     if (_isLoading || !_hasMore) return;
 
     _isLoading = true;
@@ -30,21 +30,14 @@ class PlayersTournamentsCubit extends Cubit<PlayersTournamentsState> {
       final result = await tournamentRepository.fetchPlayersTournaments(
         player1Id: player1.playerId,
         player2Id: player2.playerId,
-        limit: limit,
-        startAfter: _lastDocument,
+        page: _currentPage,
       );
 
-      final newTournaments = result['tournaments'] as List<Tournament>;
-      _lastDocument = result['lastDocument'] as DocumentSnapshot?;
-
-      final allTournaments = [...state.tournaments, ...newTournaments];
-
-      if (newTournaments.isEmpty) {
-        _hasMore = false;
-      }
+      if (result.hasMore) _currentPage = _currentPage.next;
+      _hasMore = result.hasMore;
 
       emit(PlayersTournamentsState(
-        tournaments: allTournaments,
+        tournaments: [...state.tournaments, ...result.items],
         isLoading: false,
       ));
     } catch (e) {
