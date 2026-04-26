156ab6c9b82759ed2605ba0e533c0cea7211fe6f
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-04-26 12:35:06 +0300

    refactor(home): add DI to LiveStreamMatches

--- lib/presentation/widgets/home_widgets/live_stream_matches.dart
+++ lib/presentation/widgets/home_widgets/live_stream_matches.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tennis_cup/data/data_providers/tournament_api.dart';
+import 'package:tennis_cup/core/di/service_locator.dart';
+import 'package:tennis_cup/data/models/match.dart';
 import 'package:tennis_cup/data/models/tournament.dart';
-import 'package:tennis_cup/data/repositories/tournament_repository.dart';
 import 'package:tennis_cup/logic/cubit/live_stream_match_index_cubit.dart';
 import 'package:tennis_cup/presentation/widgets/home_widgets/live_stream_match.dart';
-import 'package:tennis_cup/data/models/match.dart';
 
 class LiveStreamMatches extends StatelessWidget {
-  final tournamentRepository =
-      const TournamentRepository(tournamentApi: TournamentApi());
-
   final bool isScreenWide;
   const LiveStreamMatches({super.key, this.isScreenWide = false});
 
@@ -21,8 +17,8 @@ class LiveStreamMatches extends StatelessWidget {
       Match? closestMatch;
       Duration closestDuration = const Duration(days: 365000);
 
-      for (final match in tournament.matches!) {
-        Duration difference = match.dateTime.difference(DateTime.now()).abs();
+      for (final match in tournament.matches ?? []) {
+        final difference = match.dateTime.difference(DateTime.now()).abs();
         if (difference < closestDuration) {
           closestDuration = difference;
           closestMatch = match;
@@ -47,7 +43,7 @@ class LiveStreamMatches extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final liveStreamMatchesTournaments =
-        tournamentRepository.fetchLiveStreamMatchesTournaments();
+        ServiceLocator.tournamentRepository.fetchLiveStreamMatchesTournaments();
 
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -75,11 +71,12 @@ class LiveStreamMatches extends StatelessWidget {
                 return const Center(child: CircularProgressIndicator());
               }
               if (snapshot.hasData) {
-                if (snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No matches found'));
-                }
                 final tournaments = snapshot.data!;
                 final matches = _getMatchesToDisplay(tournaments);
+
+                if (matches.isEmpty) {
+                  return const Center(child: Text('No matches found'));
+                }
 
                 return PageView.builder(
                   scrollDirection:
