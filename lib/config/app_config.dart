ef9ff021495a0dc5c2f49b7b416dab07ce844a02
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-05-19 17:30:48 +0300

    infra: migrate to prod

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,29 +1,4 @@
 const gatewayUrl = String.fromEnvironment(
   'GATEWAY_URL',
-  defaultValue: 'http://localhost:8080',
-);
-
-const playerServiceUrl = String.fromEnvironment(
-  'PLAYER_URL',
-  defaultValue: 'http://localhost:8082',
-);
-
-const tournamentServiceUrl = String.fromEnvironment(
-  'TOURNAMENT_URL',
-  defaultValue: 'http://localhost:8084',
-);
-
-const arenaServiceUrl = String.fromEnvironment(
-  'ARENA_URL',
-  defaultValue: 'http://localhost:8083',
-);
-
-const authServiceUrl = String.fromEnvironment(
-  'AUTH_URL',
-  defaultValue: 'http://localhost:8081',
-);
-
-const matchServiceUrl = String.fromEnvironment(
-  'MATCH_URL',
-  defaultValue: 'http://localhost:8085',
+  defaultValue: 'https://api-gateway-1050273358137.europe-central2.run.app',
 );
