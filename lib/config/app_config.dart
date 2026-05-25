d79dca8b9b5b824f8035237433c3b938406baa4b
Maksym Tiutiunnyk <tiutiunnyk.kpi@gmail.com>
2026-05-25 21:55:20 +0300

    chore: add commented local gatewayUrl

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -7,3 +7,13 @@ const wsUrl = String.fromEnvironment(
   'WS_URL',
   defaultValue: 'wss://api-gateway-1050273358137.europe-central2.run.app/ws',
 );
+
+// const gatewayUrl = String.fromEnvironment(
+//   'GATEWAY_URL',
+//   defaultValue: 'http://localhost:8080',
+// );
+
+// const wsUrl = String.fromEnvironment(
+//   'WS_URL',
+//   defaultValue: 'ws://localhost:8080/ws',
+// );
